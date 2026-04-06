562de727474186ea921814edd105af17cc07bbf3
HarunSuleiman <harunsuleiman55@gmail.com>
2026-04-06 12:01:53 +0300

    Home page styling

--- frontend/front_end/src/pages/Home.jsx
+++ frontend/front_end/src/pages/Home.jsx
@@ -1,6 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="Home">
+      <h1>Home</h1>
+
+      <ul>
+        <li>
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+        </li>
+
+        <li>
+          <Link to="/add_employee">
+            <button>Add Employee</button>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
+
 export default Home;
